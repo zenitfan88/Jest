47949c0b8d23fa9aa8cc04d4086c2d745e4539f2
zenitfan88 <nikanenkov@rambler.ru>
2023-01-25 21:54:11 +0400

    init commit

--- booksApp/src/utils/read-file.js
+++
@@ -1,9 +0,0 @@
-export default function readFile(file) {
-  return new Promise((resolve) => {
-    const reader = new FileReader();
-    reader.readAsDataURL(file);
-    reader.onload = () => {
-      resolve(reader.result)
-    };
-  })
-}
